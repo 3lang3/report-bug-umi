e6c430c74b0bf01eb6b79db47bbd6e1eb7898348
3lang <675483520@qq.com>
2021-07-06 10:59:18 +0800

    feat: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import be from 'braft-editor'
+import styles from './index.less';
+
+console.log(be)
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+}
